84a96631e1d33b55549b08cf1909c6fac7bf1443
Frank <frank.snelling03@gmail.com>
2024-09-30 14:47:34 +0100

    basic GUI format created

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# create graph structure
-
-class Node:
-    """Defines a single node in the graph."""
-    def __init__(self, value):
-        self.value = value
+import matplotlib.pyplot as plt
+import networkx as nx
+import tkinter as tk
+from tkinter import Frame
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class Graph:
     """Defines a structure of connected nodes (undirected) with an adjacency list."""
@@ -12,8 +11,7 @@ class Graph:
         
     def add_node(self, value):
         """Adds new node to the graph and returns Node object"""
-        new_node = Node(value)
-        self.graph[new_node] = [] # neighbouring nodes represented in list
+        self.graph[value] = [] # neighbouring nodes represented in list
     
     def is_node(self, node):
         """Checks if node is in graph, returns True if it is, False if not."""
@@ -23,11 +21,61 @@ class Graph:
         """Adds edge between start_node and end_node. If either node does not exist the node is created."""
         
         # check for existance of nodes, and add as necessary
-        if not start_node.is_node():
+        if not self.is_node(start_node):
             self.add_node(start_node)    
-        if not end_node.is_node():
+        if not self.is_node(end_node):
             self.add_node(end_node)
         
         # graph is undirected, so for both nodes, add neighbouring node and weight of edge
         self.graph[start_node].append((end_node, weight)) 
         self.graph[end_node].append((start_node, weight))
+    
+    def draw(self):
+        """Draws graph using matplotlib."""
+        
+        # Build graph using networkx
+        G = nx.Graph() # created empty graph
+        
+        for node in self.graph: 
+            G.add_node(node)
+        
+        for node, edges in self.graph.items():
+            for neighbour, weight in edges:
+                G.add_edge(node, neighbour, weight=weight)
+        
+        return G
+                
+
+class GraphApp:
+    """Tkinter application to display the graph"""
+    
+    def __init__(self, master):
+        self.master = master
+        self.graph_frame = Frame(master)
+        self.graph_frame.pack()
+        self.draw()
+    
+    def draw(self):
+        myGraph = Graph()
+        myGraph.add_edge('A', 'B', 10)
+        myGraph.add_edge('C', 'B', 20)
+        myGraph.add_edge('D', 'A', 5)
+        myGraph.add_edge('A', 'E', 10)
+        myGraph.add_edge('C', 'D', 10)
+        G = myGraph.draw()
+        
+        fig, ax = plt.subplots(figsize=(5, 4))  # Set the size of the figure
+        pos = nx.spring_layout(G)
+        weights = nx.get_edge_attributes(G, 'weight')
+        
+        nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=200, font_size=10, font_weight='bold', edge_color='gray', ax=ax)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=weights, ax=ax)
+        
+        canvas = FigureCanvasTkAgg(fig, master=self.graph_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GraphApp(root)
+    root.mainloop()
